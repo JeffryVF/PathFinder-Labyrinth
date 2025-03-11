074d0a10d8ce2166027c1c6ef1c535bd683ffcc1
Rodrigo <113404331+RodrigoLopez16@users.noreply.github.com>
2025-03-11 13:05:21 -0600

    Modificado el codigo para salvar el recorrido para llegar al tesoro

--- PathFinder-Labyrinth/Funciones.h
+++ PathFinder-Labyrinth/Funciones.h
@@ -25,11 +25,12 @@ bool limitado(vector < vector<char>>& T, int i, int j) {
 /*si encuentro el elemento buscado devolver lo encontre
 		sino va a devolver no encontre*/
 
-bool algoritmo_bfs(vector <vector<char>> R, int i, int j) {
+bool algoritmo_bfs(vector <vector<char>> R, int i, int j, vector<pair<int, int>>& camino) {
 
 	/*condicion de para de la llamada recursiva*/
 
 	if (R[i][j] == 't') {
+		camino.push_back({ i,j });
 		return true;
 	}
 	if (R[i][j] == 'X') {
@@ -40,10 +41,10 @@ bool algoritmo_bfs(vector <vector<char>> R, int i, int j) {
 	/*condicion de visitado*/
 
 	R[i][j] = 'X';
+	camino.push_back({ i,j });
 
 	/*direccionamiento*/
 
-	R[i][j] = 'X';	// VISITADO
 
 	// Moverme en el mapa
 
@@ -55,14 +56,14 @@ bool algoritmo_bfs(vector <vector<char>> R, int i, int j) {
 
 		// (adj_i,adj_j) nueva posicion casilla
 		// cout << "Y: " << adj_i << " " << " X: " << adj_j << "\n";
-		if (limitado(R, adj_i, adj_j) and algoritmo_bfs(R, adj_i, adj_j)) {
+		if (limitado(R, adj_i, adj_j) and algoritmo_bfs(R, adj_i, adj_j, camino)) {
 
 
 			return true;
 		}
 
 	}
-
+	camino.pop_back();
 	return false;	// No he sido capaz de llegar a ningún tesoro
 }
 
@@ -93,21 +94,27 @@ void print_map(vector<vector<char>> R) {
 	}
 }
 
-void save_results(int x, int y, bool encontrado) {
+void save_results(int x, int y, bool encontrado, vector<pair<int, int>> camino) {
 	ofstream archivo("resultados.txt", ios::app); // Modo append para no sobrescribir
 	if (!archivo) {
 		cerr << "Error al abrir el archivo.\n";
 		return;
 	}
 
+	archivo << "Posición inicial: (" << x << ", " << y << ")\n";
 	if (encontrado) {
-		archivo << "Posición (" << x << ", " << y << ") llegó al tesoro.\n";
+		archivo << "Llegó al tesoro. Recorrido:\n";
+		for (auto& paso : camino) {
+			archivo << "(" << paso.first + 1 << ", " << paso.second + 1 << ") -> ";
+		}
+		archivo << "TESORO\n";
 	}
 	else {
-		archivo << "Posición (" << x << ", " << y << ") NO llegó al tesoro.\n";
+		archivo << "No llegó al tesoro.\n";
 	}
+	archivo << "----------------------\n";
 
-	archivo.close(); // Importante cerrar el archivo
+	archivo.close();  
 }
 
 void grafo_dfs() {
@@ -154,14 +161,14 @@ void grafo_dfs() {
 	cout << "Ingrese la fila y columna inicial: \n";
 	cin >> r >> c;
 
-
-	bool respuesta = algoritmo_bfs(R, r - 1, c - 1);	// retornara true si puedo llegar a un tesoro desde (r,c)
+	vector<pair<int, int>> camino;
+	bool respuesta = algoritmo_bfs(R, r - 1, c - 1, camino);	// retornara true si puedo llegar a un tesoro desde (r,c)
 	// en caso contrario, retornara false
 
-	if (respuesta) cout << "yes";
-	else cout << "no";
+	if (respuesta) cout << "Has llegado al tesoro!";
+	else cout << "No has llegado al tesoro!";
 	cout << endl;	// salto de linea
-	save_results(r, c, respuesta);
+	save_results(r, c, respuesta, camino);
 	
 }
 
